2ea84ab2028b1a102d95865e12eff72c199baa9f
Sascha Ritter <ritter@traum-ferienwohnungen.de>
2020-03-21 19:52:35 +0100

    Fixed TwilioClientTest

--- src/test/java/de/hilfstelefon/backend/service/TwilioClientTest.java
+++ src/test/java/de/hilfstelefon/backend/service/TwilioClientTest.java
@@ -4,10 +4,6 @@ import de.hilfstelefon.backend.domain.TwilioCall;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 @QuarkusTest
 class TwilioClientTest {
 
@@ -19,8 +15,7 @@ class TwilioClientTest {
 
     private TwilioCall createTestCall() {
         TwilioCall call = new TwilioCall();
-        call.callSid = "CAab570cc846ddd75aa77c62e24b826c25";
-        call.accountSid = "ACb1d198ab96bb32bc506f9ee5878839b4";
+        call.callsid = "CAab570cc846ddd75aa77c62e24b826c25";
 
         return call;
     }
