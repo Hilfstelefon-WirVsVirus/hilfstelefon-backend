b1e6a3304e4780bb3f6e6a048610118d46bf22f3
Tom Beyer <beyer@traum-ferienwohnungen.de>
2020-03-21 17:58:55 +0100

    Add further status change callback endpoints

--- src/main/java/de/hilfstelefon/backend/resource/twilio/CallStatusCallback.java
+++ src/main/java/de/hilfstelefon/backend/resource/twilio/CallStatusCallback.java
@@ -3,17 +3,15 @@ package de.hilfstelefon.backend.resource.twilio;
 import de.hilfstelefon.backend.domain.TwilioCall;
 import de.hilfstelefon.backend.events.TwilioCallCompleted;
 import io.vertx.core.eventbus.EventBus;
-import org.joda.time.DateTime;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.time.ZonedDateTime;
 
-@Path("/twilio/status")
+@Path("/twilio/status/call")
 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-public class StatusCallback {
+public class CallStatusCallback {
 
     private static final String STATUS_COMPLETED = "completed";
 
@@ -31,7 +29,7 @@ public class StatusCallback {
             @FormParam("Timestamp") String timestamp,
             @FormParam("CallStatus") String callStatus
     ) {
-        if (! callStatus.equals(StatusCallback.STATUS_COMPLETED)) {
+        if (! callStatus.equals(CallStatusCallback.STATUS_COMPLETED)) {
             System.out.println("Unexpected status update: " + callStatus);
             return;
         }
---
+++ src/main/java/de/hilfstelefon/backend/resource/twilio/RecordingStatusCallback.java
@@ -0,0 +1,32 @@
+package de.hilfstelefon.backend.resource.twilio;
+
+import de.hilfstelefon.backend.domain.TwilioCall;
+import de.hilfstelefon.backend.events.TwilioCallCompleted;
+import io.vertx.core.eventbus.EventBus;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Path("/twilio/status/recording")
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+public class RecordingStatusCallback {
+
+    @POST
+    @Transactional
+    public void statusChanged(
+            @FormParam("CallSid") String callSid,
+            @FormParam("AccountSid") String accountSid,
+            @FormParam("Caller") String caller,
+            @FormParam("FromCity") String fromCity,
+            @FormParam("FromZip") String fromZip,
+            @FormParam("Timestamp") String timestamp,
+            @FormParam("CallStatus") String callStatus
+    ) {
+        System.out.println("Recording status updated");
+    }
+}
