c313b01dd13850c5f94611d8888b92a7934aa4d8
Sascha Ritter <ritter@traum-ferienwohnungen.de>
2020-03-22 11:09:38 +0100

    Removed custom ID for now since it led to detached entity exceptions

--- src/main/java/de/hilfstelefon/backend/domain/HelpRequest.java
+++ src/main/java/de/hilfstelefon/backend/domain/HelpRequest.java
@@ -3,14 +3,11 @@ package de.hilfstelefon.backend.domain;
 import javax.persistence.Entity;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
 public class HelpRequest extends PanacheEntity {
-
-    public UUID id;
 
     public LocalDateTime creationDate;
 
@@ -25,8 +22,4 @@ public class HelpRequest extends PanacheEntity {
     public Category category = Category.SUPPORT;
 
     public Status status = Status.OPEN;
-
-    public HelpRequest() {
-        this.id = UUID.randomUUID();
-    }
 }
