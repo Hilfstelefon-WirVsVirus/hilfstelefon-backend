734335f4d8d4753b94f9b294214899fa014b1e16
Sascha Ritter <ritter@traum-ferienwohnungen.de>
2020-03-22 14:11:54 +0100

    Optimized imports

--- src/main/java/de/hilfstelefon/backend/resource/twilio/TranscriptionStatusCallback.java
+++ src/main/java/de/hilfstelefon/backend/resource/twilio/TranscriptionStatusCallback.java
@@ -1,11 +1,5 @@
 package de.hilfstelefon.backend.resource.twilio;
 
-import de.hilfstelefon.backend.domain.TwilioCall;
-import de.hilfstelefon.backend.events.TwilioCallCompleted;
-import de.hilfstelefon.backend.events.TwilioRecordingCompleted;
-import de.hilfstelefon.backend.events.TwilioTranscriptionCompleted;
-import io.vertx.core.eventbus.EventBus;
-
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
@@ -13,6 +7,9 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+
+import de.hilfstelefon.backend.events.TwilioTranscriptionCompleted;
+import io.vertx.core.eventbus.EventBus;
 
 @Path("/twilio/status/transcription")
 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
