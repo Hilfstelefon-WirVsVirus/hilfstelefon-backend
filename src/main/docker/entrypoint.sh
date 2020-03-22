6d98b40c7427e6d9802fd1bf6b277a7cca2b04a5
Nico Windler <nico.windler@gmail.com>
2020-03-22 17:17:22 +0100

    Add twilio secret

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-secrets=$PG_SECRETS
-export DB_URL=$(echo $secrets | jq -r .DB_URL)
-export DB_PASSWORD=$(echo $secrets | jq -r .DB_PASSWORD)
+secretsPG=$PG_SECRETS
+export DB_URL=$(echo $secretsPG | jq -r .DB_URL)
+export DB_PASSWORD=$(echo $secretsPG | jq -r .DB_PASSWORD)
+
+secretsTWILIO=$TWILIO_SECRETS
+export TWILIO_PASSWORD=$(echo $secretsTWILIO | jq -r .TWILIO_PASSWORD)
+
 /deployments/run-java.sh
